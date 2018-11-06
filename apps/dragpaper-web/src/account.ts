5290140670df4c8190fafb2c2bb61f707e666dd1
fragment0 <hi@fragment0.com>
2018-11-06 16:41:01 +0800

    move login request to fetch

--- apps/dragpaper-web/src/account.ts
+++ apps/dragpaper-web/src/account.ts
@@ -5,15 +5,16 @@ export function login(ctx: IRouterContext) {
   return readConfig().then((config: any) => {
     const {account} = config
     const {body} = <any>ctx.request
+    ctx.set('content-type', 'application/json')
     if (account.username !== body.username) {
-      return ctx.redirect('/')
+      return ctx.body = {code: 1001, err: 'username or password not match'}
     }
     if (account.password !== md5(body.password)) {
-      return ctx.redirect('/')
+      return ctx.body = {code: 1001, err: 'username or password not match'}
     }
     ctx.cookies.set('st', account.password, {
       httpOnly: true,
     })
-    ctx.redirect('/')
+    ctx.body = {code: 0}
   })
 }
